995e16062098c07249d23951a75ff3b754259313
qxchuckle <1934009145@qq.com>
2023-09-28 00:42:58 +0800

    修改卡片样式

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -4,13 +4,14 @@
       <div class="admin-container">
         <n-empty description="你什么也找不到" v-if="!shareList.length"></n-empty>
         <div class="card-box" v-if="shareList.length">
-          <n-card v-for="item in shareList" class="share-card" @click="toShare(item)" size="small" hoverable>
+          <n-card v-for="(item, index) in shareList" class="share-card" @click="toShare(item)" size="small" hoverable>
             <div class="title">标题：{{ item.title ? item.title : '无' }}</div>
             <div class="content">内容：{{ item.content }}</div>
-            <n-space align="center">
+            <n-space align="center" class="info">
               <div>语言：{{ item.language ? item.language : 'Text' }}</div>
               <div>时间：{{ dayjs(Number(item.time)).format("YYYY-MM-DD HH:MM") }}</div>
             </n-space>
+            <div class="floor">{{ index + 1 }}</div>
           </n-card>
         </div>
       </div>
@@ -89,6 +90,9 @@ const toShare = (item) => {
     .share-card {
       margin-bottom: 10px;
       width: calc(50% - 8px);
+      position: relative;
+      line-height: 1;
+      cursor: pointer;
 
       .content {
         word-break: break-all;
@@ -96,6 +100,21 @@ const toShare = (item) => {
         -webkit-box-orient: vertical;
         -webkit-line-clamp: 1;
         overflow: hidden;
+        margin: 6px 0 5px;
+      }
+
+      .floor {
+        color: #555555;
+        z-index: 0;
+        position: absolute;
+        top: 2px;
+        right: 5px;
+        font-style: italic;
+        font-size: 26px;
+        line-height: 1;
+        z-index: 0;
+        font-weight: normal;
+        opacity: 0.6;
       }
     }
 
