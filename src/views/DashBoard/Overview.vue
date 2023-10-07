710188ee64bda4bfa0dc667e661f4eebbefb6258
qxchuckle <1934009145@qq.com>
2023-10-07 12:53:13 +0800

    更新

--- src/views/DashBoard/Overview.vue
+++ src/views/DashBoard/Overview.vue
@@ -15,47 +15,15 @@ import OverviewCards from '@/components/Admin/Overview/OverviewCards.vue';
 import OverviewCharts from '@/components/Admin/Overview/OverviewCharts.vue';
 
 onBeforeMount(async () => {
+  await getData();
+});
+
+const getData = async () => {
   loadingBar.start();
   await viewStore.getView();
   loadingBar.finish();
-});
-
-// import { use } from 'echarts/core'
-// import { BarChart } from 'echarts/charts'
-// import {
-//   TitleComponent,
-//   TooltipComponent,
-//   GridComponent
-// } from 'echarts/components'
-// import { CanvasRenderer } from 'echarts/renderers'
-
-// use([TitleComponent, TooltipComponent, GridComponent, BarChart, CanvasRenderer])
-
-// const sharesByDayChart = computed(() => {
-//   return {
-//     title: { text: "分享统计图/天" },
-//     tooltip: {},
-//     xAxis: {
-//       data: viewStore.sharesByDay.map(item => item.day).reverse(),
-//     },
-//     yAxis: {},
-//     series: [
-//       {
-//         name: "分享数量",
-//         type: "bar",
-//         data: viewStore.sharesByDay.map(item => item.shares.length).reverse(),
-//       },
-//     ],
-//   }
-// });
-
-
-
-
-
+}
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
