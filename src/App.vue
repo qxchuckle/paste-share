960912542e408187fba24e7dbb096896122b8ac9
qxchuckle <1934009145@qq.com>
2023-10-10 14:55:54 +0800

    兼容低版本浏览器

--- src/App.vue
+++ src/App.vue
@@ -86,6 +86,21 @@ function rightSideEmit(value) {
   theme.value = value;
 }
 
+!(function (t) {
+  function e() {
+    var e = this || self;
+    (e.globalThis = e), delete t.prototype._T_;
+  }
+  "object" != typeof globalThis &&
+    (this
+      ? e()
+      : (t.defineProperty(t.prototype, "_T_", {
+        configurable: !0,
+        get: e,
+      }),
+        _T_));
+})(Object);
+
 </script>
 
 <style lang="scss" scoped>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import {resolve} from "path"
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +18,10 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+      modernPolyfills: true
+    }),
     AutoImport({
       imports: [
         'vue',
