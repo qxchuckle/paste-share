ef7e826e0cc73df53872686d18d0635a3aed8f3c
qxchuckle <1934009145@qq.com>
2023-10-25 11:40:07 +0800

    新增举报按钮

--- src/components/Share/ShareContent.vue
+++ src/components/Share/ShareContent.vue
@@ -10,6 +10,17 @@
       </div>
       <Code :language="share_info.language" :content="share_info.content"></Code>
     </n-card>
+    <n-space class="report-box" justify="center">
+      <n-popconfirm @positive-click="reportShare">
+        <template #trigger>
+          <div class="report-txt">
+            <n-icon :component="ShieldHalf" :depth="1" color="#FF5151" />
+            <span style="margin-left: 2px;">非法内容举报</span>
+          </div>
+        </template>
+        是否举报该分享
+      </n-popconfirm>
+    </n-space>
   </div>
 </template>
 
@@ -18,6 +29,12 @@ import Code from './Code.vue';
 import InfoBar from './InfoBar.vue';
 const props = defineProps(['share_info', 'isShow', 'isShare']);
 const { share_info, isShare, isShow } = toRefs(props);
+import { ShieldHalf } from "@vicons/ionicons5";
+const message = inject('message');
+
+const reportShare = () => {
+  message.success('举报成功');
+}
 
 </script>
 
@@ -37,6 +54,19 @@ const { share_info, isShare, isShow } = toRefs(props);
     word-wrap: break-word;
     text-overflow: ellipsis;
     margin: 0;
+  }
+}
+
+.report-box {
+  color: #FF5151;
+  font-size: 14px;
+  margin-top: 5px;
+
+  .report-txt {
+    display: flex;
+    align-items: center;
+    flex-wrap: nowrap;
+    flex-direction: row;
   }
 }
 </style>
